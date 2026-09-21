40ba46f67ea164be33f0644d3880058d1b28d329
ljunn <ljpeng1995@gmail.com>
2026-09-21 13:51:04 +0000

    Keep mobile site dialogs within the dynamic viewport

--- frontend/src/components/common/BaseDialog.vue
+++ frontend/src/components/common/BaseDialog.vue
@@ -11,7 +11,7 @@
         @click.self="handleClose"
       >
         <!-- Modal panel -->
-        <div ref="dialogRef" :class="['modal-content', widthClasses]" @click.stop>
+        <div ref="dialogRef" :class="['modal-content max-h-[95dvh] sm:max-h-[90dvh]', widthClasses]" @click.stop>
           <!-- Header -->
           <div class="modal-header gap-3">
             <h3 :id="dialogId" class="modal-title min-w-0 [overflow-wrap:anywhere]">
